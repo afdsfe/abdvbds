a9856d12593b25a167ce7de541ba776864964e33
BizData-Analytics <107223145+BizData-Analytics@users.noreply.github.com>
2023-02-12 22:28:36 -0500

    1

---
+++ page1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def page1():
+    """Create the content for page 1 of the website."""
+    st.write("This is the content for page 1.")
+    st.write("You can add any type of content here, such as text, images, videos, etc.")
+    st.write("This is a good place to add more information about a specific topic or feature.")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,40 @@
 import streamlit as st
-
+from PIL import Image
 def header():
     """Create a header for the website."""
+    logo_image = st.image(r"C:/Users/ABC/Documents/GitHub/abdvbds/bear.jpg", use_column_width=False, width=100)
     st.header("Header")
 
 def footer():
     """Create a footer for the website."""
-    st.footer("Footer")
+    st.write("Footer")
 
 def main_content():
     """Create the main content area of the website."""
     st.write("Main Content")
 
+from page1 import page1
+
+def page2():
+    """Create the second page of the website."""
+    st.write("Page 2")
+
+def page3():
+    """Create the third page of the website."""
+    st.write("Page 3")
+
 def run_app():
     """Run the Streamlit app."""
     header()
-    main_content()
+    page = st.sidebar.selectbox("Select a page", ["Main Content", "Page 1", "Page 2", "Page 3"])
+    if page == "Main Content":
+        main_content()
+    elif page == "Page 1":
+        page1()
+    elif page == "Page 2":
+        page2()
+    elif page == "Page 3":
+        page3()
     footer()
 
 if __name__ == "__main__":
