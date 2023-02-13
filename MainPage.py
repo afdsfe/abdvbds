0408b2202c980d9913713994284bb2ce5c02ffd1
BizData-Analytics <107223145+BizData-Analytics@users.noreply.github.com>
2023-02-13 00:47:49 -0500

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -6,7 +6,7 @@ def main_content():
     #st.write("Data Analysis Projects")
     st.markdown(
         """
-        ### Welcome to the Data Analysis Projects section!
+        ## Welcome to the Data Analysis Projects section!
 
 
         * **Predictive modeling:** Build models to make predictions based on historical data.
@@ -17,15 +17,15 @@ def main_content():
 
         Here, I showcase some of the data analysis projects I have worked on.
 
-        ## Project 1: Predicting Sales Trends
+        #### Project 1: Predicting Sales Trends
 
         In this project, I built a predictive model using historical sales data to forecast future sales trends. I used machine learning algorithms such as linear regression and decision trees to make accurate predictions. I also visualized the results using `matplotlib` and `seaborn` to give a clear understanding of the sales trends over time.
 
-        ## Project 2: Exploring Customer Segmentation
+        #### Project 2: Exploring Customer Segmentation
 
         In this project, I performed exploratory data analysis (EDA) on customer data to identify different segments of customers based on their demographics and purchasing behavior. I used `pandas` and `numpy` to manipulate and analyze the data and `seaborn` to visualize the results. The insights gained from this project were used to inform targeted marketing efforts.
 
-        ## Project 3: Improving Supply Chain Efficiency
+        #### Project 3: Improving Supply Chain Efficiency
 
         In this project, I used data from a logistics company to analyze and optimize their supply chain processes. I cleaned and preprocessed the data, and then used time-series analysis and simulation to identify bottlenecks and inefficiencies in the supply chain. I presented the results and recommended solutions to the company to improve their overall efficiency.
         """
